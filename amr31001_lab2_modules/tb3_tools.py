087300579eb3f370e0f4fe4e70a3560a22edc170
Tom Howard <t.howard@sheffield.ac.uk>
2025-11-18 09:35:43 +0000

    Add `Lidar()` class

--- amr31001_lab2_modules/tb3_tools.py
+++ amr31001_lab2_modules/tb3_tools.py
@@ -1,6 +1,7 @@
 from rclpy.node import Node
 from geometry_msgs.msg import TwistStamped, Quaternion
 from nav_msgs.msg import Odometry
+from sensor_msgs.msg import LaserScan
 from math import atan2, asin, degrees
 import numpy as np
 
@@ -110,4 +111,61 @@ class Pose():
     def round(self, value, precision):
         value = int(value * (10**precision))
         return float(value) / (10**precision)
+
+class Lidar():
+    def __init__(self, node: Node):
+        self.distance = self.scanSubsets(node)
+        self.subscriber = node.create_subscription(
+            msg_type=LaserScan,
+            topic='/scan',
+            callback=self.laserscan_cb,
+            qos_profile=10
+        )  
+
+    class scanSubsets():
+        def __init__(self, node: Node):
+            self.node = node
+            self.front = 0.0
+            self.r1 = 0.0; self.r2 = 0.0; self.r3 = 0.0; self.r4 = 0.0
+            self.l1 = 0.0; self.l2 = 0.0; self.l3 = 0.0; self.l4 = 0.0
         
+        def show(self):
+            lidar_segs = f"              l1     front     r1          \n" \
+                         f"       l2     {self.l1:<5.3f}  {self.front:^5.3f}  {self.r1:>5.3f}     r2       \n" \
+                         f"l3     {self.l2:<5.3f}                       {self.r2:>5.3f}   r3\n" \
+                         f"{self.l3:<5.3f}                                   {self.r3:>5.3f}\n" \
+                         f"{self.l4:<5.3f} <-- l4                     r4 --> {self.r4:>5.3f}"
+            self.node.get_logger().info(
+                    f"Lidar distances (m):\n"
+                    f"{lidar_segs}"
+                )
+                        
+    def laserscan_cb(self, scan_data: LaserScan):
+
+        def subset_average(start_index, stop_index):
+            range = np.array(scan_data.ranges[start_index: stop_index+1])
+            valid_data = range[range>0.1]
+            return valid_data.mean() if np.shape(valid_data)[0] > 0 else np.nan
+
+        # front:
+        left_arc = scan_data.ranges[0:20+1]
+        right_arc = scan_data.ranges[-20:]
+        full_arc = np.array(left_arc[::-1] + right_arc[::-1])
+        valid = full_arc[full_arc>0.1]
+        self.distance.front = valid.mean() if np.shape(valid)[0] > 0 else np.nan
+        
+        # right subsets:
+        self.distance.r1 = subset_average(320, 340)
+        self.distance.r2 = subset_average(300, 320)
+        self.distance.r3 = subset_average(275, 290)
+        self.distance.r4 = subset_average(250, 265)
+        
+        # left subsets:
+        self.distance.l1 = subset_average(20, 40)
+        self.distance.l2 = subset_average(40, 60)
+        self.distance.l3 = subset_average(70, 85)
+        self.distance.l4 = subset_average(95, 110)
+        
+        self.wait_for_readings = False
+
+    
