d7df680f45c9a8cbd405ca32fb391ce583ddb668
Tom Howard <t.howard@sheffield.ac.uk>
2025-12-15 17:02:51 +0000

    Launch description working

--- launch/ex4_setup.launch.py
+++ launch/ex4_setup.launch.py
@@ -1,15 +1,13 @@
 from launch_ros.actions import Node
 
 from launch import LaunchDescription
-from launch.actions import (ExecuteProcess, LogInfo, 
-                            RegisterEventHandler, TimerAction)
+from launch.actions import ExecuteProcess, LogInfo, RegisterEventHandler
 from launch.event_handlers import OnProcessExit
 from launch.substitutions import FindExecutable
 from ament_index_python.packages import get_package_share_directory
 
 def generate_launch_description():
-    this_pkg_share = get_package_share_directory("amr31001_lab2")
-   
+ 
     img_capture_node = Node(
         package='amr31001_lab2',
         executable='ex4.py',
@@ -21,22 +19,20 @@ def generate_launch_description():
         cmd=[[
             FindExecutable(name='ros2'),
             ' run tuos_examples image_colours.py ',
-            this_pkg_share,
+            get_package_share_directory("amr31001_lab2"),
             '/images/cam_img.jpg '
         ]],
         shell=True
     )
     
     return LaunchDescription([
+        img_capture_node,
         RegisterEventHandler(
             OnProcessExit(
                 target_action=img_capture_node,
                 on_exit=[
-                    LogInfo('launch image processing...'),
-                    TimerAction(
-                        period=2.0,
-                        actions=[spawn_image_processing],
-                    )
+                    LogInfo(msg='launching image_colours.py...'),
+                    spawn_image_processing
                 ]
             )
         ),
